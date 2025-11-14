e12a7441a5860dc4496ce9e5af8becfeabf98649
GlaceYTT <official.glaceyt@gmail.com>
2025-11-15 02:43:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -30,9 +30,9 @@ class EnterpriseConfigurationManager {
              * Configure your Lavalink server for audio processing
              */
           lavalink: {
-                host: EnvironmentVariableProcessor.LAVALINK_HOST || "192.99.10.133", 
-                port: EnvironmentVariableProcessor.LAVALINK_PORT || 5140,       
-                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "friston", 
+                host: EnvironmentVariableProcessor.LAVALINK_HOST || "us-01.strixnodes.com", 
+                port: EnvironmentVariableProcessor.LAVALINK_PORT || 8003,       
+                password: EnvironmentVariableProcessor.LAVALINK_PASSWORD || "glace", 
                 secure: EnvironmentVariableProcessor.LAVALINK_SECURE === 'true' || false
             },
             
@@ -101,6 +101,7 @@ module.exports = primaryApplicationConfiguration;
  * Never share your bot token or database URI publicly!
  * Use environment variables in production!
  */
+
 
 
 
